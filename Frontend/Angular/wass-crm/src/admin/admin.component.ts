140a42f8cf50d477e5ea1d6efd47e8eb9faffbb6
Chris <mavroboticswebdev7690@gmail.com>
2025-02-18 17:30:13 -0500

    No still

--- Frontend/Angular/wass-crm/src/admin/admin.component.ts
+++ Frontend/Angular/wass-crm/src/admin/admin.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarAdminComponent } from '@shared/components/navbar-admin';
-import { SidebarAdminComponent } from '@shared/components/sidebar-admin/sidebar-admin.component';
 
 @Component({
   selector: 'comp-admin',
   standalone: true,
-  imports: [RouterOutlet, NavbarAdminComponent, SidebarAdminComponent],
+  imports: [RouterOutlet],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
--- Frontend/Angular/wass-crm/src/admin/dashboard/dashboard.component.ts
+++ Frontend/Angular/wass-crm/src/admin/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarAdminComponent } from '@shared/components/navbar-admin';
 
 @Component({
   selector: 'comp-dashboard',
   standalone: true,
-  imports: [NavbarAdminComponent],
+  imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
